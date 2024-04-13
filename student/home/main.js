5da4717ddda5247b2cc4008f4bf67a9813008a0a
arvinjayguno <arvinguno73@gmail.com>
2024-04-13 13:13:43 +0800

    9-arvin

---
+++ student/home/main.js
@@ -0,0 +1,8 @@
+function toggleLyrics() {
+    var lyricsContainer = document.getElementById("lyrics-container");
+    if (lyricsContainer.style.display === "none") {
+      lyricsContainer.style.display = "block";
+    } else {
+      lyricsContainer.style.display = "none";
+    }
+  }
